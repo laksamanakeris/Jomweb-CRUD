66cccd5c7fa7630eea4b197708a7b89f15c75289
Laksamana Keris <kerismelayu@gmail.com>
2013-05-24 17:40:55 +0800

    Updated list.php

--- list.php
+++ list.php
@@ -2,8 +2,18 @@
 <html>
 <head>
 	<title>JOMWEB : CRUD Challenge</title>
+	<link rel="stylesheet" href="assets/normalize.css">
+	<link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+
+	<div class="wrapper">
 	<h1>Senarai Maklumat Pelajar</h1>
+
+
+	
+	</div>
+
+
 </body>
 </html>
